8e0840e5c16377c34dc20706e6762280a490b7f8
John Schoeman <johnschoeman1617@gmail.com>
2017-12-02 12:41:39 -0500

    Create simplest redux app

---
+++ redux_workspace/egghead_redux/index.js
@@ -0,0 +1,27 @@
+const defaultState = 0;
+
+const counter = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      break;
+  }
+}
+
+const { createStore } = Redux;
+const store = createStore(counter, 0);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const render = () => {
+    document.body.innerText = store.getState();
+  }
+  store.subscribe(render)
+  render();
+  
+  document.addEventListener('click', () => {
+    store.dispatch({type: 'INCREMENT'})
+  })
+})
