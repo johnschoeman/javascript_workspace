fa98775dcd31a9763dbdae65947cb7b0d2e14497
John Schoeman <johnschoeman1617@gmail.com>
2018-01-15 18:56:46 -0500

    Complete rn navigation quick start

--- react_native_workspace/react_navigator/rn-navigation-quick-start/App.js
+++ react_native_workspace/react_navigator/rn-navigation-quick-start/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import RootNavigator from './src/navigation/RootNavigator';
+import RootTabs from './src/navigation/RootTabs';
+import RootDrawer from './src/navigation/RootDrawer';
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <RootDrawer />
     );
   }
 }
