1ff35edfa4eb63d8a3579fe25bec9aa766c54559
John Schoeman <johnschoeman1617@gmail.com>
2019-10-13 09:06:06 -0400

    module 1

---
+++ testing_javascript/01_fundementals/03_testing-framework.js
@@ -0,0 +1,35 @@
+const { sum, subtract } = require("./math");
+
+test("sum", () => {
+  const result = sum(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
+
+test("subtract", () => {
+  const result = subtract(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
+
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`√ ${title}`);
+  } catch (error) {
+    console.error(`x ${title}`);
+    console.error(error);
+  }
+}
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+    toEqual(expected) {},
+    toBeGreaterThan(expected) {}
+  };
+}
---
+++ testing_javascript/01_fundementals/04_async-await.js
@@ -0,0 +1,49 @@
+const { sum, subtract, sumAsync, subtractAsync } = require("./math");
+
+test("sumAsync", async () => {
+  const result = await sumAsync(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
+
+test("subtractAsync", async () => {
+  const result = await subtractAsync(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
+
+// doenst work
+test("sum", () => {
+  const result = sum(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
+
+// doenst work
+test("subtract", () => {
+  const result = subtract(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
+
+async function test(title, callback) {
+  try {
+    await callback();
+    console.log(`√ ${title}`);
+  } catch (error) {
+    console.error(`x ${title}`);
+    console.error(error);
+  }
+}
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+    toEqual(expected) {},
+    toBeGreaterThan(expected) {}
+  };
+}
